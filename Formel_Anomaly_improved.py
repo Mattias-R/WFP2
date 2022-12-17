e514ea90ef3dc75deea308b8ede78f6fe4fa334d
Mattias <mattias.r@gmx.at>
2022-12-17 17:25:34 +0100

    updated :)

--- Formel_Anomaly_improved.py
+++ Formel_Anomaly_improved.py
@@ -47,32 +47,35 @@ for device in excelrange:
         else:
             difference = round(float(tableName[i].value) - float(tableName[i-1].value) ,2)
             value.append(difference)
-        #if value is 0, that means the customer is proably on holidy
-        #if we do not pop it, the allowed difference would be too low and more false positive apppear
+        # if value is 0, that means the customer is probably on holiday
+        # if we do not pop it, the allowed difference would be too low and more false positive appear
         if(value[len(value)-1] == 0):
             value.pop()
-        #calculates the durchschnitt
+        # calculates the durchschnitt
         durchschnitt = round(float(sum(value) / float(len(value))), 2)
         for k in value:
-            #calculates the abweichung based on the formula of https://welt-der-bwl.de/Mittlere-absolute-Abweichung#:~:text=Die%20mittlere%20absolute%20Abweichung%20vom%20Median%20ist%3A%20(%20%7C%201%2D,Standardabweichung.
+            # calculates the abweichung based on the formula of https://welt-der-bwl.de/Mittlere-absolute-Abweichung#:~:text=Die%20mittlere%20absolute%20Abweichung%20vom%20Median%20ist%3A%20(%20%7C%201%2D,Standardabweichung.
             abweichung = abweichung + abs(k - durchschnitt)
         abweichung = round(abweichung / float(len(value)),2)
 
-        #if difference is greater then durchschnitt + abweichung * 2 for 3 days in a row, we concider it as an anomaly
-        #why 3 days in a row? because otherwise a single increase would an error. this would raise the error counter to over 200
-        #why *2? because we need some buffer to use more water. like when it is summer or something like that
+        # if difference is greater than durchschnitt + abweichung * 2 for 3 days in a row, we concider it as an anomaly
+        # why 3 days in a row? because otherwise a single increase would an error. this would raise the error counter to over 200
+        # why *2? because we need some buffer to use more water. like when it is summer or something like that
         if(difference > (durchschnitt + abweichung)*2 and value[len(value) - 2] > (durchschnitt + abweichung)*2 and value[len(value) - 3] > (durchschnitt + abweichung)*2):
-            x = [i , round(durchschnitt + abweichung, 2), difference]
+            x = [i, round(durchschnitt + abweichung, 2), difference]
             printErrors.append(x)
             count = count + 1
-    #this if statement is only to print the relevant information (only columns with errors)
+    # this if statement is only to print the relevant information (only columns with errors)
     if not printErrors:
         continue
     print("Spalte: " + str(device) + ", Errors: " + str(len(printErrors)) + ", List: " + str(printErrors))
-print("All errors: ",count)
+print("All errors: ", count)
 
-#---------------------- RULE CHAIN -----------------------
-#Daten bereingen. die if mit WM brauchen wir nicht, genauso wie die Fehlerwerte mit über 3000. aber alle NA müssen rausgefiltert werden..
-#Wichtig ist das die Datenbank folgende infos beihaltet. Aktueller wert beziehungsweise alle werte. ein Wert indem wir den Durchschnitt speichern können. Timestamp ist auch wichtig.#
-#Value kommt rein. wird in die Datenbank hinzugefügt. Datenbank kalkuiert aufs neue den Durchschnitt etc. danach wird überprüft und gegebenenfalls eine Notification gesendet
-#Es muss difference UND der gesamt Wert in der Datenbank gespeichert werden. Um bessere Sichtbarkeit zu gewährleisten.
+# ---------------------- RULE CHAIN -----------------------
+# Daten bereingen. die if mit WM brauchen wir nicht, genauso wie die Fehlerwerte mit über 3000. aber alle NA müssen rausgefiltert werden..
+# Wichtig ist das die Datenbank folgende infos beihaltet. Aktueller wert beziehungsweise alle werte. ein Wert indem wir den Durchschnitt speichern können. Timestamp ist auch wichtig.#
+# Value kommt rein. wird in die Datenbank hinzugefügt. Datenbank kalkuiert aufs neue den Durchschnitt etc. danach wird überprüft und gegebenenfalls eine Notification gesendet
+# Es muss difference UND der gesamt Wert in der Datenbank gespeichert werden. Um bessere Sichtbarkeit zu gewährleisten.
+
+# maschine learning?
+# lineare regression?
